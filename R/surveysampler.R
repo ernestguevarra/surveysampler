70521403f55e82c8f6b5609d192d11ff6c7cf7ac
Ernest Guevarra <ernest@guevarra.io>
2021-08-18 13:03:04 +0100

    initial commit

---
+++ R/surveysampler.R
@@ -0,0 +1,18 @@
+################################################################################
+#
+#'
+#' Survey Sampling and Analysis Tools
+#'
+#' Studies that evaluate survey sampling and analysis approaches require varied
+#' techniques and methods not found in a single package in R. This package
+#' provides utilities that aid and simplify these approaches to enable
+#' streamlined assessment and comparison of different survey sampling and
+#' analysis techniques.
+#'
+#' @docType package
+#' @name zscorer
+#' @keywords internal
+#'
+#
+################################################################################
+"_PACKAGE"
