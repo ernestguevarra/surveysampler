9763364e71ec5e9fdf186917d650c83b7998c4b5
Ernest Guevarra <ernest@guevarra.io>
2021-08-22 21:30:59 +0100

    add test for create_sample_psm function

--- tests/testthat/test-create_sample_psm.R
+++ tests/testthat/test-create_sample_psm.R
@@ -1,16 +1,12 @@
-sim_survey <- accept_reject_psu(
+sim_survey <- create_sample_psm(
   x = village_list,
   svy = sample_data,
   psu = c("id", "psu"),
   match = "cluster",
-  pop = "population",
-  verbose = FALSE,
-  show_plot = FALSE
+  pop = "population"
 )
 
-## Test if output is a data.frame
+
 test_that("output is a data.frame", {
   expect_s3_class(sim_survey, "data.frame")
 })
-
-
