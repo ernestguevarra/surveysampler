8aa049736abeeea7d80935df158e53285793677c
Ernest Guevarra <ernest@guevarra.io>
2021-08-22 21:17:06 +0100

    create test for accept_reject_psu

---
+++ tests/testthat/test-accept_reject_sample.R
@@ -0,0 +1,14 @@
+sim_survey <- accept_reject_psu(
+  x = village_list,
+  svy = sample_data,
+  psu = c("id", "psu"),
+  match = "cluster",
+  pop = "population",
+  verbose = FALSE,
+  show_plot = FALSE
+)
+
+## Test if output is a data.frame
+test_that("output is a data.frame", {
+  expect_s3_class(sim_survey, "data.frame")
+})
