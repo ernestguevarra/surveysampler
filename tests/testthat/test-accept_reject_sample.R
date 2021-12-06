472fce74305389f95558badf92ac87cc87cc4424
Ernest Guevarra <ernest@guevarra.io>
2021-12-06 10:39:11 +0000

    add tests to increase coverage

--- tests/testthat/test-accept_reject_sample.R
+++ tests/testthat/test-accept_reject_sample.R
@@ -52,3 +52,25 @@ test_that("output has the correct structure", {
   expect_true(all(names(svy) %in% names(sim_survey)))
   expect_equal(names(svy), names(sim_survey))
 })
+
+## Change variables name of village list
+x <- village_list
+names(x) <- c("id", "villageName", "population", "psu")
+
+svy <- sample_data
+names(svy) <- c("surveydate", "psu", "sex", "birthdate", "age",
+                "weight", "height", "oedema", "muac", "measure", "clothes")
+
+sim_survey <- accept_reject_psu(
+  x = x,
+  svy = svy,
+  psu = c("id", "psu"),
+  pop = "population",
+  verbose = FALSE,
+  show_plot = FALSE
+)
+
+test_that("output has the correct structure", {
+  expect_true(all(names(svy) %in% names(sim_survey)))
+  expect_equal(names(svy), names(sim_survey))
+})
